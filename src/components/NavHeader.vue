0ec619c9315a37e94c4ee1299ebb818de7ce95d1
Sebastian Sforzini <sebastian@traveldepartment.ie>
2023-09-23 01:33:04 +0100

    New Header Changes I've added al the required changes for the header. I've also included the logo.

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,77 @@
+<template>
+
+    <div>
+        <b-navbar toggleable="lg" variant="light" class="mt-2">
+            <b-navbar-brand href="#">
+                <b-img src="/src/assets/SS_Logo1.png" id="web_logo" fluid alt="Logo"></b-img>
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav class="header-align">
+                <b-navbar-nav>
+                    <b-nav-item href="#">About Me</b-nav-item>
+                    <b-nav-item href="#">Skills</b-nav-item>
+                    <b-nav-item href="#">Career</b-nav-item>
+                    <b-nav-item href="#">Testimonials</b-nav-item>
+                    <b-nav-item href="#">Contact</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<style scoped>
+
+    .header-align {
+        justify-content: right;
+    }
+
+    .header-align .nav-item {
+        margin-right: 2rem;
+    }
+
+    .navbar-brand {
+        margin-left: 3rem;
+    }
+
+    .navbar {
+        background-color: white !important;
+    }
+
+    @media only screen and (max-width: 1182px) {
+        .navbar-brand {
+            width: 39%;
+        }
+    }
+
+    @media only screen and (max-width: 991px) {
+        .navbar-collapse {
+            padding-left: 1rem;
+            margin-top: 1rem;
+            background-color: #F4EBDB;
+        }
+
+        .navbar-toggler {
+            margin-right: 1rem;
+        }
+
+        .navbar-brand {
+            margin-left: 1rem;
+            width: auto;
+        }
+    }
+
+    @media only screen and (max-width: 690px) {
+        .navbar {
+            display: inline-table;
+        }
+
+        .navbar-toggler {
+            float: right;
+        }
+    }
+
+</style>
+<script setup>
+</script>
