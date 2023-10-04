415f112a74390c2c4b96b16aa58d19c22f70b7f1
Sebastian Sforzini <sebastian@traveldepartment.ie>
2023-10-04 16:23:37 +0100

    Pre - Production Fixing the router and adding some build options

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,9 @@ export default defineConfig({
     server: {
         port: 8080,
         hot: true
+    },
+    scripts: {
+        build: "vite build",
+        preview: "vite preview"
     }
 })
